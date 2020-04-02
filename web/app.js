581980c0486710770b7da800e0db78974ab49a56
Cody Benkoski <cob322@lehigh.edu>
2020-04-02 15:50:43 -0400

    Fixed https issue

--- web/app.js
+++ web/app.js
@@ -20,7 +20,7 @@ let tile_server_url_light = "https://tiles.codyben.me/styles/positron/{z}/{x}/{y
 var tile_server_url_mapbox = "https://api.mapbox.com/styles/v1/bencodyoski/ck83ddg6u5xa91ipc15icdk21/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmVuY29keW9za2kiLCJhIjoiY2s1c2s0Y2JmMHA2bzNrbzZ5djJ3bDdscyJ9.7MuHmoSKO5zAgY0IKChI8w";
 let tile_server_url_dark = "https://tiles.codyben.me/styles/dark-matter/{z}/{x}/{y}.png";
 let route_server_url = "https://routeserver.codyben.me/";
-let tile_server_url = "http://tiles.codyben.me/styles/osm-bright/{z}/{x}/{y}.png";
+let tile_server_url = "https://tiles.codyben.me/styles/osm-bright/{z}/{x}/{y}.png";
 
 
 
