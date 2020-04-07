0c9e4dacfdb55e7d53d2ad1295f00a7daf10c37f
Cody Benkoski <cob322@lehigh.edu>
2020-04-07 19:50:11 -0400

    Did some JS refactoring

--- web/js/app.js
+++ web/js/app.js
@@ -1,28 +1,4 @@
-/*
-
-some variable declarations
-
-*/
-isFirstRun = true; //is this the first time page has been loaded
-marker_obj = {}; //store bus markers/locations (for updating)
-route_to_use = null; //used in if statements for knowing which routes to use
-var stop_arr = {}; //holds location of each bus stop
-
-let query_string = window.location.search; //get query string from url
-let args = new URLSearchParams(query_string);
-var tile_style = {}; //holds different tile styles.
-var curr_style = "light"
-
-
-
-//routes
-// let tile_server_url_light = "https://tiles.codyben.me/styles/positron/{z}/{x}/{y}.png";
-var tile_server_url_mapbox = "https://api.mapbox.com/styles/v1/bencodyoski/ck83ddg6u5xa91ipc15icdk21/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmVuY29keW9za2kiLCJhIjoiY2s1c2s0Y2JmMHA2bzNrbzZ5djJ3bDdscyJ9.7MuHmoSKO5zAgY0IKChI8w";
-// let tile_server_url_dark = "https://tiles.codyben.me/styles/dark-matter/{z}/{x}/{y}.png";
-// let route_server_url = "https://routeserver.codyben.me/";
-let tile_server_url = "https://tiles.codyben.me/styles/osm-bright/{z}/{x}/{y}.png";
-
-
+/* DEFINE TILE LAYERS */
 
 // tile_style['dark'] = L.tileLayer(tile_server_url_dark, { //takes tile server URL and will return a tile
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -50,6 +26,10 @@ if (window.innerWidth > 600) {
     icb = [96, 96]
 }
 
+/* END */
+
+
+/* DRAW THE MAP */
 
 function sync_callback(data) {
     json = data;
---
+++ web/js/config.js
@@ -0,0 +1,23 @@
+/*
+
+some variable declarations
+
+*/
+isFirstRun = true; //is this the first time page has been loaded
+marker_obj = {}; //store bus markers/locations (for updating)
+route_to_use = null; //used in if statements for knowing which routes to use
+var stop_arr = {}; //holds location of each bus stop
+
+let query_string = window.location.search; //get query string from url
+let args = new URLSearchParams(query_string);
+var tile_style = {}; //holds different tile styles.
+var curr_style = "light"
+
+
+
+//routes
+// let tile_server_url_light = "https://tiles.codyben.me/styles/positron/{z}/{x}/{y}.png";
+var tile_server_url_mapbox = "https://api.mapbox.com/styles/v1/bencodyoski/ck83ddg6u5xa91ipc15icdk21/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmVuY29keW9za2kiLCJhIjoiY2s1c2s0Y2JmMHA2bzNrbzZ5djJ3bDdscyJ9.7MuHmoSKO5zAgY0IKChI8w";
+// let tile_server_url_dark = "https://tiles.codyben.me/styles/dark-matter/{z}/{x}/{y}.png";
+// let route_server_url = "https://routeserver.codyben.me/";
+let tile_server_url = "https://tiles.codyben.me/styles/osm-bright/{z}/{x}/{y}.png";
