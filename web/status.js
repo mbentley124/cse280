4cb1942dbebcabd3556f4638ad9ed696241ec4a7
Cody Benkoski <cob322@lehigh.edu>
2020-04-07 19:39:31 -0400

    Simple status check script

---
+++ web/status.js
@@ -0,0 +1,37 @@
+service_status = {"tileserver":false, "routeserver":false};
+
+
+
+function populate_true(service) {
+	service_status[service] = true;
+}
+
+function check_tileserver() {
+	$.ajax({
+	  url: "https://tiles.codyben.me",
+	  success: function() {
+	  	populate_true('tileserver');
+	  },
+	});
+}
+
+function check_routeserver() {
+	$.ajax({
+	  url: "https://routeserver.codyben.me/route/v1/driving/-75.374375,40.607656;-75.375347,40.603158",
+	  success: function() {
+	  	populate_true('tileserver');
+	  },
+	});
+}
+
+function check_all() {
+	
+	$.each(service_status, function(k,v) {
+	if(!v) {
+		console.warn("Service "+k+" appears to be down.");
+	}
+});
+}
+
+check_routeserver();
+check_tileserver();
