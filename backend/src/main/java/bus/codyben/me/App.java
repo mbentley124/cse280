82ad24caa6d3db187c328fcfc35117656f097ba4
Michael Bentley <mbentley124@gmail.com>
2020-04-09 17:07:22 -0700

    Created sample db query

--- backend/src/main/java/bus/codyben/me/App.java
+++ backend/src/main/java/bus/codyben/me/App.java
@@ -1,6 +1,14 @@
 package bus.codyben.me;
 
 import spark.Spark;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -11,6 +19,20 @@ import spark.Route;
  */
 public class App {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); 
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/busapp?serverTimezone=UTC", "busapp", "busapp");) {
+            PreparedStatement preparedStatement = conn.prepareStatement("select distinct route_id from lehighbusdata");
+            ResultSet results = preparedStatement.executeQuery();
+            while (results.next()) {
+                System.out.println(results.getInt("route_id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("Hello World!");
         Spark.port(4567);
         Spark.get("/test", (req, res) -> {
