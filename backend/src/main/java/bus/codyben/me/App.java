41fd9cc1ba60ab196798c0def2ed85e5e226324a
Joseph Malisov <j.a.malisov@gmail.com>
2020-09-09 19:44:26 -0400

    added comments to App.java

--- backend/src/main/java/bus/codyben/me/App.java
+++ backend/src/main/java/bus/codyben/me/App.java
@@ -1,3 +1,4 @@
+//imports
 package bus.codyben.me;
 
 import spark.Spark;
@@ -14,16 +15,24 @@ import spark.Response;
 import spark.Route;
 
 /**
+ * Boilerplate code for a Java Spark web server.
+ */
+
+/**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
+
+        //import the jdbc driver
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); 
         } catch (ClassNotFoundException e1) {
             e1.printStackTrace();
         }
+
+        //connect to db
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/busapp?serverTimezone=UTC", "busapp", "busapp");) {
             PreparedStatement preparedStatement = conn.prepareStatement("select distinct route_id from lehighbusdata");
             ResultSet results = preparedStatement.executeQuery();
@@ -33,8 +42,12 @@ public class App {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println("Hello World!");
-        Spark.port(4567);
+
+        
+        // System.out.println("Hello World!");
+        Spark.port(4567); //the port to use
+
+        //boilerplate. Will be replaced by our actual routes.
         Spark.get("/test", (req, res) -> {
             System.out.println("Hi");
             return "";
