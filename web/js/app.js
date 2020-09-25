e695443815ab80bef4e1317df47771b880d3a3e3
hnl221 <hnl221@lehigh.edu>
2020-09-25 10:35:33 -0400

    fixed some ids

--- web/js/app.js
+++ web/js/app.js
@@ -539,7 +539,7 @@ $.each(keys, function() {
     var count = 0;
     $('#init-stop-list').append('<a id="transportation-item" class="pure-menu-link" onclick="show_stops(\'' + this + '\')">' + this.charAt(0).toUpperCase() + this.slice(1) + '</a>');
     $('#init-stop-list').append('<div id="stops-list-container-' + this + '" style="display: none;"><ul class="pure-menu-list" id="stops-list-' + this + '" style="background-color: rgb(153, 67, 6); font-size: 15px; overflow-x: hidden; overflow-y: scroll; max-height: 52.2vh;"></ul></div>');
-    $('#stops-list-container-' + bus).prepend('<div style="text-align:center;border-bottom: 1px solid white; height:33.6px;"><input type="text" id="search" class="stops-item" style="margin-top:5px; width: 90%;" placeholder="Look for a stop" onkeypress="render_search_results('+this+')"/></div>');
+    $('#stops-list-container-' + bus).prepend('<div style="text-align:center;border-bottom: 1px solid white; height:33.6px;"><input type="text" id="search-'+ this +'" class="stops-item" style="margin-top:5px; width: 90%;" placeholder="Look for a stop" onkeypress="render_search_results(\''+this+'\')"/></div>');
     $.each(stops[this], function() {
         if (!stops_tracker.has(this.name)) {
             $('#stops-list-' + bus).append('<li><a class="pure-menu-link stops-item" onclick="find_stop(' + this.latitude + ',' + this.longitude + ',\''+this.name+'\')">' + this.name + '</a></li>');
