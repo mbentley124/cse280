a3e22f228a74ff634672b66732a59d2487794072
Joseph Malisov <j.a.malisov@gmail.com>
2020-10-09 10:42:04 -0400

    created icon.js file and moved icon info out of app.js

---
+++ web/js/icons.js
@@ -0,0 +1,75 @@
+/*
+
+images for icons
+
+*/
+
+var you = L.icon({
+    iconUrl: 'img/you.png',
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var fw = L.icon({
+    iconUrl: 'img/FW.jpeg',
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var bstop = L.icon({
+    iconUrl: 'img/busstop.png',
+
+    iconSize: icb, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+var pe = L.icon({
+    iconUrl: 'img/PE.jpeg',
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var lu = L.icon({
+    iconUrl: 'img/LU.jpeg',
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var cc = L.icon({
+    iconUrl: 'img/CC.jpeg',
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+const lanta = L.icon({
+    iconUrl: make_icon_bus("#FFFFFF", "#1500ff"),
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+const lehigh = L.icon({
+    iconUrl: make_icon_bus("#FFFFFF", "#68310A"),
+
+    iconSize: ic, // size of the icon
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+const new_position = L.icon({
+    iconUrl: make_icon_bus("#dbddde7d", "#dbddde"),
+    iconSize: ic,
+});
