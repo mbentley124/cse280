0ce315dc1a88bd62e8a4756af96cbe73ca669a23
HanSoon98 <hansen.luk.98@gmail.com>
2020-10-30 12:16:09 -0400

    added undefined check to bus drawing

--- web/js/app.js
+++ web/js/app.js
@@ -243,12 +243,17 @@ function draw_buses(bus_obj, map) {
         if ((timings == null) || (timings.length == 0)) {
             popup_content = `${service} Bus: ${bus_id} <br>On route: ${typeof mapped_routes.get(route_id) === "undefined" ? route_id : mapped_routes.get(route_id)} <br> Previous stop: ${last_stop}`;
         } else {
-            const { minutes, seconds, total_time } = next_time;
-            let time_str = `${minutes} minutes & ${seconds} seconds.`;
-            if (minutes == 0 && seconds < 20) {
-                time_str = "Arriving Soon.";
+            if (typeof next_time !== 'undefined' 
+                && typeof next_time.minutes !== 'undefined' 
+                && typeof next_time.seconds !== 'undefined'){
+                const { minutes, seconds, total_time } = next_time;
+                let time_str = `${minutes} minutes & ${seconds} seconds.`;
+                if (minutes == 0 && seconds < 20) {
+                    time_str = "Arriving Soon.";
+                }
+                popup_content = `${service} Bus: ${bus_id} <br>On route: ${typeof mapped_routes.get(route_id) === "undefined" ? route_id : mapped_routes.get(route_id)} <br> <a onclick="zoom_to_stop('${last_stop}')" href="#stop-${last_stop}"> ${last_stop}</a> => <a onclick="zoom_to_stop('${next_stop}');" href="#stop-${next_stop}">${next_stop}</a> in ${time_str}`;
             }
-            popup_content = `${service} Bus: ${bus_id} <br>On route: ${typeof mapped_routes.get(route_id) === "undefined" ? route_id : mapped_routes.get(route_id)} <br> <a onclick="zoom_to_stop('${last_stop}')" href="#stop-${last_stop}"> ${last_stop}</a> => <a onclick="zoom_to_stop('${next_stop}');" href="#stop-${next_stop}">${next_stop}</a> in ${time_str}`;
+            console.error("next_time has undefined property");
         }
         marker.setPopupContent(popup_content);
         buses_running.add(bus_id);
