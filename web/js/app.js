1d1650c5a14fd342a7de4905e21be08615fb61e1
HanSoon98 <hansen.luk.98@gmail.com>
2020-11-13 12:56:08 -0500

    resolve iOS styling issues

--- web/js/app.js
+++ web/js/app.js
@@ -504,8 +504,6 @@ $('<div id="directions_instructions" style="padding: 2%; color: whitesmoke;"><p>
 $("#directions_tab").toggle();
 $("#directions_instructions").toggle();
 
-
-
 for (var id of lehigh_route_stop_ids) {
     for (var j of stops.lehigh) {
         if (j.stop_id == id) {
